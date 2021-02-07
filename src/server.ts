4d413a47bec594ad05009ae242a3d3e713d68dba
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2021-02-07 11:45:16 -0300

    added body parser

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import bodyParser from 'body-parser';
 
 const server = express();
+
+server.use(bodyParser.json());
 
 server.get("/", (request, response) => {
     return response.send("Hello!");
